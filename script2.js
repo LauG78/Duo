b0aa5b49147b8db4ff633cb5bb715160765532fd
Dimitri Bertolami <dimibertolami@gmail.com>
2022-06-01 16:51:47 +0200

    first changes are done

---
+++ script2.js
@@ -0,0 +1,18 @@
+// let favoriteColor = document.getElementById("color").innerHTML = "";
+//spirit animal just a small adjustment! most info is already correct!
+let spiritAnimal = document.getElementById("lanecat");
+spiritAnimal.src = "images2/correctSpiritAnimal.jpg";
+spiritAnimal.width = "200";
+spiritAnimal.height = "450";
+
+//spirit animal text adjust
+let spirit = document.getElementById("spirit");
+let spiritTxt = '<em><span style="font-weight: 700">Dimi always loved </span></em>cats. The cat is his spiritual animal and yes just any cat'
+spirit.innerHTML = spiritTxt;
+
+//sports
+document.getElementById("elephant").src = 'images2/zorb.jpg';
+let sport = document.getElementById("sport");
+alert(sport);
+console.log(sport);
+
