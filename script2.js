91e0f5113574dcc37a2f72c2a4dbb2f00772a6f0
Dimitri Bertolami <dimibertolami@gmail.com>
2022-06-03 11:49:19 +0200

    finished more or less

--- script2.js
+++ script2.js
@@ -33,39 +33,132 @@ description.innerHTML = `<div id="description"><p>In three words Dimi is: <ul><l
 //challenge
 let challenge = document.getElementById("challenge");
 challenge.innerHTML = `<div class="col-12 box" id="boxA" style="background: greenyellow"><div class="chaser" style="background: red"></div></div>`
-////////////////////////////////////////////
-// Chaser
-////////////////////////////////////////////
-// chaser circle
-let boxA = document.querySelector('.box');
-let circle = document.getElementById('circle');
-circle.width = 30+'px';
-circle.height = 30+'px';
+// ////////////////////////////////////////////
+// // Chaser
+// ////////////////////////////////////////////
+// // chaser circle
+// let boxA = document.querySelector('.box');
+// let circle = document.getElementById('circle');
+// circle.width = 30+'px';
+// circle.height = 30+'px';
+//
+// boxA.addEventListener('mousemove', (e) => {
+//     let left = e.offsetX;
+//     let top = e.offsetY;
+//     circle.style.left = `${left - 30}px`
+//     circle.style.top = `${top - 20}px`
+//
+// })
+//
+// circle.style.left = x + "px";
+// circle.style.top = y + "px";
 
-boxA.addEventListener('mousemove', (e) => {
-    let left = e.offsetX;
-    let top = e.offsetY;
-    circle.style.left = `${left - 30}px`
-    circle.style.top = `${top - 20}px`
 
-})
+const canvas = document.querySelector("#canvas")
+const ctx = canvas.getContext('2d');
 
-circle.style.left = x + "px";
-circle.style.top = y + "px";
+// To set width and height of current viewport do
+canvas.width  = window.innerWidth   // canvas.width  = 900
+canvas.height = window.innerHeight    // canvas.height = 600
+// x = 10
+// y = 20
 
-function draw()
-{
-    var canvas = document.getElementById('circle');
-    if (canvas.getContext)
-    {
-        var ctx = canvas.getContext('2d');
-        var X = canvas.width / 2;
-        var Y = canvas.height / 2;
-        var R = 45;
-        ctx.beginPath();
-        ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
-        ctx.lineWidth = 3;
-        ctx.strokeStyle = '#FF0000';
-        ctx.stroke();
+
+
+const width = 800;
+const height= 400;
+drawRectangle((canvas.width/2)-(width/2), (canvas.height/2)-(height/2), width, height, 'yellow', '');
+
+drawRectangle(210, 10, 60, 100,  '','red');
+drawRectangle(215, 15, 50, 90,  '','orange');
+drawRectangle(220, 20, 40, 80,  '','yellow');
+drawRectangle(225, 25, 30, 70,  '','green');
+drawRectangle(230, 30, 20, 60,  '','blue');
+drawRectangle(235, 35, 10, 50,  '','indigo');
+drawRectangle(240, 40, 3, 40,  'violet','');
+
+drawCircle(300, 300, 75, 'red');
+drawCircle(300, 300, 70, 'orange');
+drawCircle(300, 300, 65, 'yellow');
+drawCircle(300, 300, 60, 'green');
+drawCircle(300, 300, 55, 'blue');
+drawCircle(300, 300, 50, 'indigo');
+drawCircle(300, 300, 45, 'violet');
+
+let a = [400, 200];   // [x, y]
+let b = [420, 290];   // [x, y]
+let c = [600, 300];   // [x, y]
+drawTriangle(a, b, c);
+
+
+
+function drawTriangle(a, b, c){
+    ctx.beginPath();
+    console.log(`A=${a} B=${b} C=${c}`);
+    alert(`A=${a[0]}  -  B=${a[1]}`);
+    drawLine(a[0],a[1], b[0],b[1], color);    //drawLine(xFrom, yFrom, xTo, yTo, color="DeepPink")
+    drawLine(b[0], b[1], c[0], c[1], color);
+    drawLine(c[0], c[1], a[0], a[1], color);
+    ctx.closePath()
+}
+
+/********************************************************************************
+*   drawRectangle Function as written for Becode Class 2022 @ Antwerpen
+*   -------------------------------------------------------------------
+* Usage: drawRectangle (x, y, width, height, color, fillColor)
+* ------
+* x and y are self explanatory width and height also.
+ *
+ * Added: x= center and y= center
+ *         if(x === 'center' || y === 'center'){
+ *             ctx.strokeRect((canvas.width/2)-(width/2), (canvas.height/2)-(height/2), width, height, color);
+ *         } else {
+ *             ctx.strokeRect(x, y, width, height, Color);
+ *         }
+ *
+* Color is a bit adapted to use include fillColor
+ ******************************************************************************** */
+function drawRectangle(x=10, y=20, width=200, height=300, color='red', fillColor=''){
+    // Allways Step 1. fire up the beginPath method
+        ctx.beginPath()
+    if(fillColor === ''){
+        // pencil style to just stroke (no fill)
+        ctx.strokeStyle = color;
+        // draw rectangle without fill Color
+            ctx.strokeRect(x, y, width, height, color);
+    } else {
+        // draw rectangle with fill color!
+        ctx.fillStyle = fillColor;
+        ctx.fillRect(x, y, width, height, '', fillColor);
     }
+}       //function drawRectangle
+
+
+/*****************************************************************************************
+*           Function drawCircle as written for Becode Class 2022 @ Antwerpen
+*         ------------------------------------------------------------------
+*   usage:    drawCircle(x, y, radius, color)
+*   ------
+*****************************************************************************************/
+function drawCircle(x=300, y=300, radius= 75, color=''){
+    // Step 1. Invoke beginPath method
+    ctx.beginPath()
+    // Step 2. create arc with start angle =  0      end angle = 360degrees or 2*PI
+    ctx.arc(x, y, radius, 0, 2*Math.PI)
+    // Fill style with Lavender color
+    ctx.fillStyle = color;
+    //Step 3. Now ink on canvas
+    ctx.fill()
+}
+function drawLine(xFrom, yFrom, xTo, yTo, color="DeepPink"){
+    // Step 1. Invoke beginPath method
+    ctx.beginPath();
+    // Point A
+    ctx.moveTo(xFrom, yFrom);
+// Point B
+    ctx.lineTo(xTo, yTo);
+
+    ctx.strokeStyle = color;
+    ctx.stroke();
+
 }
